5ccb9a9a199878d027327b65efa35769a3ef9c55
AnishDe12020 <63192115+AnishDe12020@users.noreply.github.com>
2021-07-28 15:51:45 +0530

    Made a third page for testing purposes

---
+++ src/pages/page-3.js
@@ -0,0 +1,12 @@
+import * as React from "react"
+
+import Layout from "../components/layout"
+
+
+const ThirdPage = () => (
+    <Layout>
+        <h1>Hi from the third page</h1>
+    </Layout>
+)
+
+export default ThirdPage
