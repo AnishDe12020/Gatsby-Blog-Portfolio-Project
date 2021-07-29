abad783ef6509b999766e932e4530ffb7ec85b70
AnishDe12020 <63192115+AnishDe12020@users.noreply.github.com>
2021-07-29 14:37:13 +0530

    Added markdown file support and a few example markdown files

--- gatsby-config.js
+++ gatsby-config.js
@@ -15,6 +15,13 @@ module.exports = {
         path: `${__dirname}/src/images`,
       },
     },
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `markdowns`,
+        path: `${__dirname}/src/markdown-pages`,
+      },
+    },
     `gatsby-transformer-sharp`,
     `gatsby-plugin-sharp`,
     {
