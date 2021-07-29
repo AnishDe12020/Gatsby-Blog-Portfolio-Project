9e31f832b74f06c71fe80af0781541dbe49b00a3
AnishDe12020 <63192115+AnishDe12020@users.noreply.github.com>
2021-07-29 15:48:57 +0530

    made the blog post pages

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,4 +1,5 @@
 const { createFilePath } = require(`gatsby-source-filesystem`)
+const path = require(`path`)
 
 exports.onCreateNode = ({ node, getNode, actions }) => {
     const { createNodeField } = actions
@@ -11,3 +12,57 @@ exports.onCreateNode = ({ node, getNode, actions }) => {
         })
     }
 }
+
+exports.createPages = ({ graphql, actions }) => {
+    const { createPage } = actions
+    return graphql(`
+    {
+        allMarkdownRemark {
+            edges {
+            node {
+                fields {
+                   slug
+                }
+            }
+            }   
+        }
+}  
+    `).then(result => {
+        result.data.allMarkdownRemark.edges.forEach(({ node }) => {
+            createPage({
+                path: node.fields.slug,
+                component: path.resolve(`./src/templates/blog-post.js`),
+                context: {
+                    slug: node.fields.slug
+                }
+            })
+        })
+    })
+}
+
+// exports.createPages = ({ graphql, actions }) => {
+//     const { createPage } = actions
+//     return graphql(`
+//       {
+//         allMarkdownRemark {
+//           edges {
+//             node {
+//               fields {
+//                 slug
+//               }
+//             }
+//           }
+//         }
+//       }
+//     `).then(result => {
+//       result.data.allMarkdownRemark.edges.forEach(({ node }) => {
+//         createPage({
+//           path: node.fields.slug,
+//           component: path.resolve(`./src/templates/blog-post.js`),
+//           context: {
+//             slug: node.fields.slug,
+//           },
+//         })
+//       })
+//     })
+//   }
