bd6a06a63265e6f32d85953c4a2475efa5841b32
AnishDe12020 <63192115+AnishDe12020@users.noreply.github.com>
2021-07-29 14:54:35 +0530

    Changed the body of the home page to show blogs posts

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,29 +1,44 @@
 import * as React from "react"
-import { Link } from "gatsby"
-import { StaticImage } from "gatsby-plugin-image"
+import { Link, graphql } from "gatsby"
 
 import Layout from "../components/layout"
 import Seo from "../components/seo"
 
-const IndexPage = () => (
+const IndexPage = ({ data }) => (
   <Layout>
     <Seo title="Home" />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <StaticImage
-      src="../images/gatsby-astronaut.png"
-      width={300}
-      quality={95}
-      formats={["AUTO", "WEBP", "AVIF"]}
-      alt="A Gatsby astronaut"
-      style={{ marginBottom: `1.45rem` }}
-    />
-    <p>
-      <Link to="/page-2/">Go to page 2</Link> <br />
-      <Link to="/using-typescript/">Go to "Using TypeScript"</Link>
-    </p>
+    <div>
+      <h1>Anish's Thoughts</h1>
+    </div>
+    <h4>{ data.allMarkdownRemark.totalCount }</h4>
+    {
+      data.allMarkdownRemark.edges.map(({ node }) => (
+        <div key={node.id}>
+          <span>{ node.frontmatter.title } - { node.frontmatter.date }</span>
+          <p>{ node.excerpt }</p>
+        </div>
+      ))
+    }
   </Layout>
 )
 
 export default IndexPage
+
+export const query = graphql`
+  query MyQuery {
+    allMarkdownRemark {
+      totalCount
+      edges {
+        node {
+          id
+          frontmatter {
+            date
+            description
+            title
+          }
+          excerpt
+        }
+      }
+    }
+  }
+`
